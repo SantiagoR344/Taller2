ba46738d6ec1e8b81422610e1c14a80f5a93286f
SantiagoR344 <santiramos0630@gmail.com>
2023-04-14 18:46:13 -0500

    chages

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,18 @@
 "use strict";
+const form = document.getElementById('form');
+const btnGuardar = document.getElementById('btn-guardar');
+btnGuardar.addEventListener('click', (e) => {
+    //e.preventDefault();
+    console.log("Ingreso a la validación del formulario");
+    //Obtener los valores de los inputs
+    const tipoDocumento = document.getElementById('tipoDocumento');
+    const numeroDocumento = document.getElementById('numeroDocumento');
+    const nombre = document.getElementById('nombre');
+    const apellido = document.getElementById('apellido');
+    const celular = document.getElementById('celular');
+    const email = document.getElementById('email');
+    const linkedin = document.getElementById('linkedin');
+    const github = document.getElementById('github');
+    //La validación del formulario
+    console.log(tipoDocumento.value);
+});
